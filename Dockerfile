ca107078ea533bfde3a35776ffd31be51b9c02c3
Christian Hartl <sircanist@gmail.com>
2022-02-05 23:40:22 +0100

    update dockerfile, remove improt from policy and change iri mapper

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM maven
 COPY . /ic-owl
 COPY owls /owls
 RUN cd /ic-owl/concept-fx-dl-only && mvn install
+WORKDIR /ic-owl
 ENTRYPOINT /ic-owl/default-run.sh
--- default-run.sh
+++ default-run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 run_scenario1() {
-    bash run.sh /owls/cti2.owl /owls/cti2-scenario1.owl /owls/cti2-scenario1-bk.owl /owls/cti2-scenario1-policy.owl /owls/cti2-iri-mapper.
+    bash run.sh /owls/cti2.owl /owls/cti2-scenario1.owl /owls/cti2-scenario1-bk.owl /owls/cti2-scenario1-policy.owl /owls/cti2-iri-mapper.txt
 }
 
 run_scenario2() {
-    bash run.sh /owls/cti2.owl /owls/cti2-scenario2.owl /owls/cti2-scenario2-bk.owl /owls/cti2-scenario2-policy.owl /owls/cti2-iri-mapper.
+    bash run.sh /owls/cti2.owl /owls/cti2-scenario2.owl /owls/cti2-scenario2-bk.owl /owls/cti2-scenario2-policy.owl /owls/cti2-iri-mapper.txt
 }
 
 echo "Enter 1 for scenario 1, Enter 2 for scenario 2"
 
-read -n1 ans
+read ans
 printf "\n"
 if [ $ans -eq 1 ]
 then
